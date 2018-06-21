c1a2f287091ce9784241aecba24bac3995ddf193
Yanmao Man <dlut.manym@gmail.com>
2018-06-21 16:02:17 -0700

    enriched i3pystatus

--- .config/i3/statusbar.py
+++ .config/i3/statusbar.py
@@ -1,95 +1,23 @@
 from i3pystatus import Status
+from i3pystatus.weather import weathercom
+from i3pystatus.calendar import google
 
-status = Status()
 
-# Displays clock like this:
-# Tue 30 Jul 11:59:46 PM KW31
-#                          ^-- calendar week
+status = Status(logfile='/home/yman/var/i3pystatus.log')
+
 status.register("clock",
     format="%a %-d %b %X",)
 
-# Shows the average load of the last minute and the last 5 minutes
-# (the default value for format is used)
 status.register("load")
+status.register("temp")
 
-# Shows your CPU temperature, if you have a Intel CPU
-#status.register("temp",
-#    format="{temp:.0f}°C",)
-
-# The battery monitor has many formatting options, see README for details
-
-# This would look like this, when discharging (or charging)
-# ↓14.22W 56.15% [77.81%] 2h:41m
-# And like this if full:
-# =14.22W 100.0% [91.21%]
-#
-# This would also display a desktop notification (via D-Bus) if the percentage
-# goes below 5 percent while discharging. The block will also color RED.
-# If you don't have a desktop notification demon yet, take a look at dunst:
-#   http://www.knopwob.org/dunst/
-#status.register("battery",
-#    format="{status}/{consumption:.2f}W {percentage:.2f}% [{percentage_design:.2f}%] {remaining:%E%hh:%Mm}",
-#    alert=True,
-#    alert_percentage=5,
-#    status={
-#        "DIS": "↓",
-#        "CHR": "↑",
-#        "FULL": "=",
-#    },)
-
-# This would look like this:
-# Discharging 6h:51m
-#status.register("battery",
-#    format="{status} {remaining:%E%hh:%Mm}",
-#    alert=True,
-#    alert_percentage=5,
-#    status={
-#        "DIS":  "Discharging",
-#        "CHR":  "Charging",
-#        "FULL": "Bat full",
-#    },)
-
-# Displays whether a DHCP client is running
-#status.register("runwatch",
-#    name="DHCP",
-#    path="/var/run/dhclient*.pid",)
-
-# Shows the address and up/down state of eth0. If it is up the address is shown in
-# green (the default value of color_up) and the CIDR-address is shown
-# (i.e. 10.10.10.42/24).
-# If it's down just the interface name (eth0) will be displayed in red
-# (defaults of format_down and color_down)
-#
-# Note: the network module requires PyPI package netifaces
 status.register("network",
     interface="enp0s31f6",
     format_up="{v4} ▲:{bytes_sent} ▼:{bytes_recv}",)
 
-# Note: requires both netifaces and basiciw (for essid and quality)
-#status.register("network",
-#    interface="wlan0",
-#    format_up="{essid} {quality:03.0f}%",)
-
-# Shows disk usage of /
-# Format:
-# 42/128G [86G]
 status.register("disk",
     path="/",
-    format="{used}/{total}G [{avail}G]",)
-
-# Shows pulseaudio default sink volume
-#
-# Note: requires libpulseaudio from PyPI
-status.register("pulseaudio",
-    format="♪{volume}",)
-
-#status.register("alsa",
-#        on_leftclick = ["switch_mute"],
-#        # or as a strings without the list
-#        on_upscroll = "decrease_volume",
-#        on_downscroll = "increase_volume",
-#        # this will refresh any module by clicking on it
-#        on_rightclick = "run",)
+    format="{avail}G",)
 
 # Shows mpd status
 # Format:
@@ -101,5 +29,40 @@ status.register("pulseaudio",
 #        "play": "▶",
 #        "stop": "◾",
 #    },)
+
+status.register(
+    'weather',
+    format='{condition} {current_temp}{temp_unit}[ {update_error}]',
+    interval=900,
+    colorize=True,
+    hints={'markup': 'pango'},
+    backend=weathercom.Weathercom(
+        location_code='USAZ0247:1:US', # Tucson, AZ
+        units='imperial',
+        update_error='<span color="#ff0000">!</span>',
+    ),
+)
+
+status.register("calendar",
+    format="{title} {remaining}",
+    update_interval=10,
+    urgent_blink=True,
+    backend=google.Google(
+        credential_path='/home/yman/.gcalcli_gmail/oauth',
+        days=2))
+
+status.register("calendar",
+    format="{title} {remaining}",
+    update_interval=10,
+    urgent_blink=True,
+    backend=google.Google(
+        credential_path='/home/yman/.gcalcli_catmail/oauth',
+        days=2))
+
+status.register("cmus",
+    format="{status} {song_elapsed} {title}")
+
+status.register("pulseaudio",
+    format="♪{volume}",)
 
 status.run()
