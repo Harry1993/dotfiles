26505ee63ac9dca2e4995e69a48f1f811fd62d00
Yanmao Man <dlut.manym@gmail.com>
2018-06-27 22:21:46 -0700

    added emails to i3pystatus

---
+++ .config/i3/get_cpw.py
@@ -0,0 +1,3 @@
+class ckey:
+    def get_password(domain, user):
+        return open('/home/yman/.config/i3/cpw', 'r').read()[:-1]
---
+++ .config/i3/get_gpw.py
@@ -0,0 +1,3 @@
+class gkey:
+    def get_password(domain, user):
+        return open('/home/yman/.config/i3/gpw', 'r').read()[:-1]
